e535f2eff3e629492de45e1a90b930f4ec3495ec
andre jonas <ajonasmorais@gmail.com>
2023-09-23 10:16:43 -0300

    Configuração inicial do Spring Boot

---
+++ src/main/java/com/techcalendarapi/TechCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package com.techcalendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCalendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechCalendarApiApplication.class, args);
+	}
+
+}
